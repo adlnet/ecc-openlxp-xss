b6f7fb2831e28b64505710352d16195be4e56c9d
kjijo <kjijo@deloitte.com>
2025-07-14 09:57:02 -0500

    docker update

--- app/core/admin.py
+++ app/core/admin.py
@@ -90,7 +90,8 @@ class TermAdmin(admin.ModelAdmin):
                     'modified', )
     fieldsets = (
         (None, {'fields': ('iri', 'name', 'uuid', 'description', 'status',)}),
-        ('Info', {'fields': ('data_type', 'use', 'type', 'multiple_expected',
+        ('Info', {'fields': ('data_type', 'use', 'learning_type',
+                             'multiple_expected',
                              'source',)}),
         ('Connections', {'fields': ('term_set', 'mapping',)}),
         ('Updated', {'fields': ('updated_by',), })
